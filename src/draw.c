db60a050364f06851ef77e216dfb47a6e1066d60
Ignacio Tonoli de maussion <itonoli-@e1z2r3p25.42.us.org>
2017-07-04 19:45:09 -0700

    draw

--- src/draw.c
+++ src/draw.c
@@ -6,26 +6,39 @@
 /*   By: itonoli- <itonoli-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/04 19:20:59 by itonoli-          #+#    #+#             */
-/*   Updated: 2017/07/04 19:34:59 by itonoli-         ###   ########.fr       */
+/*   Updated: 2017/07/04 19:44:24 by itonoli-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/wolf.h"
 
+void draw_wall(t_env *e, int y)
+{
+	if (e->me_dirx < 0 && e->rayDirX < 0)
+		mlx_put_pixel(e->img, e->x, y, RED);
+	else if (e->me_dirx < 0 && e->rayDirX < 0)
+		mlx_put_pixel(e->img, e->x, y, GREY);
+	else if (e->me_dirx < 0 && e->rayDirX < 0)
+		mlx_put_pixel(e->img, e->x, y, ORANGE);
+	else if (e->me_dirx < 0 && e->rayDirX < 0)
+		mlx_put_pixel(e->img, e->x, y, GREEN);
+}
+
 void draw_lines(t_env *e)
 {
 	int y;
 
-	y = 0;
-	while (y < WIN_H)
+	y = -1;
+	while (++y < WIN_H)
 	{
 		if (y < e->draw_start)
 			mlx_put_pixel(e->img, e->x, y, SKY);
-		if (e->draw_start < y && y < e->draw_end)
-			
+		else if (e->draw_start < y && y < e->draw_end)
+			draw_wall(e, y);
+		else if (y > e->draw_end)
+			mlx_put_pixel(e->img, e->x, y, BROWN);
 	}
 }
-
 
 void	fill_img(t_env *e)
 {
